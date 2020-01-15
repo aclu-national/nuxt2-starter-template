8767500977cd118e613c77ef10ef5218fdb7e14a
Hanbyul Jo <hanbyul@nyu.edu>
2020-01-15 12:31:44 -0500

    add color file, add package-lock.json to gitignore

---
+++ template/utils/colors.js
@@ -0,0 +1,25 @@
+export const blue = '#0055aa'
+export const red = '#ef404e'
+export const redAACompliant = '#D93A3f' // WCAG AA compliant w/ white text
+
+export const lightOrange = '#FCAA17'
+export const lightYellow = '#FFE06A'
+export const lightPink = '#FABEAF'
+
+export const lightGreen = '#A7D7B5'
+export const darkGreen = '#00343A'
+export const darkPurple = '#552564'
+export const darkBurgundy = '#691B40'
+export const darkNavy = '#130F54'
+export const lightAzure = '#A3DBE3'
+
+export const lightSlate = '#EFECEA'
+export const darkSlate = '#3C3532'
+export const offblack = '#231F20'
+export const white = '#FFFFFF'
+
+export const grey10 = '#e5e5e5'
+export const grey20 = '#cccccc'
+export const grey40 = '#999999'
+export const grey60 = '#666666'
+export const grey80 = '#333333'
